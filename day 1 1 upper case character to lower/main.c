7da9c3edd528eef47c108af978493d5b1808f50a
Susmitpr <113661126+Susmitpr@users.noreply.github.com>
2023-02-25 19:43:12 +0530

    Add files via upload

---
+++ day 1 1 upper case character to lower/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<assert.h>
+int upper_to_lower(char upper);
+int main()
+{
+    char upper;
+    printf("Enter an upper case number\t");
+    scanf("%c",&upper);
+    //assert(upper_to_lower('A')=='a');
+    char low = upper_to_lower(upper);
+    printf("lower is %c",low);
+    return 0;
+}
+
+int upper_to_lower(char upper)
+{
+    char lower;
+    lower = upper+32;
+    return lower;
+
+}
+
